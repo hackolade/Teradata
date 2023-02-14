eff627001cb8fab50394c91a5a8815611a95d63c
Vitalii Bedletskyi <vitalii.bedletskyi@binary-studio.com>
2023-02-14 20:20:34 +0200

    adjust types

---
+++ reverse_engineering/helpers/indexHelper.js
@@ -0,0 +1,71 @@
+const teradataRegexConfig = require("./teradataRegexConfig");
+
+const cleanKeyName = (name = '') => {
+    return name.replace(/^("|,|\s+)+|("|,|\s+)+$/gmi, '')
+};
+const parseIndexKeys = (statement) => {
+    const mathResult = statement.match(teradataRegexConfig.indexKeyName);
+    if (!mathResult || !mathResult.length) {
+        return [];
+    }
+
+    return mathResult.map(cleanKeyName);
+};
+
+const parseHashIndexStatement = (statement) => {
+    const matchResult = teradataRegexConfig.createHashIndex.exec(statement);
+    if (!matchResult || !matchResult.groups) {
+        return {};
+    }
+
+    return {
+        indexMap: matchResult.groups.map,
+        colocateUsing: matchResult.groups.colocationName,
+        indexFallback: matchResult.groups.fallback,
+        checksum: matchResult.groups.checksum,
+        blockCompression: matchResult.groups.blockCompression,
+        indxKey: parseIndexKeys(matchResult.groups.indexColumns),
+        orderBy: matchResult.groups.orderByType,
+        orderKeys: parseIndexKeys(matchResult.groups.orderByColumns)
+    }
+};
+
+const parseJoinIndexStatement = (statement) => {
+    const matchResult = teradataRegexConfig.createJoinIndex.exec(statement);
+    if (!matchResult || !matchResult.groups) {
+        return {};
+    }
+
+    return {
+        indexMap: matchResult.groups.map,
+        colocateUsing: matchResult.groups.colocationName,
+        indexFallback: matchResult.groups.fallback,
+        checksum: matchResult.groups.checksum,
+        blockCompression: matchResult.groups.blockCompression,
+        asSelect: matchResult.groups.selectStatement,
+    }
+};
+
+const parseIndexStatement = (index) => {
+    let parsedOptions = {};
+    if (index.indexType === 'HASH') {
+        parsedOptions = parseHashIndexStatement(index.createStatement);
+    } else {
+        parsedOptions = parseJoinIndexStatement(index.createStatement);
+    }
+
+    return {
+        ...index,
+        ...parsedOptions,
+    };
+};
+
+const parseTableIndexes = (tableName, indexes) => {
+    return indexes
+        .filter(index => index.tableName === tableName)
+        .map(parseIndexStatement);
+};
+
+module.exports = {
+    parseTableIndexes,
+};
