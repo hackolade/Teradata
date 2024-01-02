f694e2c2f1218290c4b342f2cded1ab3be450f2b
Nazar Kovtun <88377450+WilhelmWesser@users.noreply.github.com>
2024-01-02 16:49:48 +0200

    HCK-4263: fixed issue with invalid cross-schema view ddl (#19)

--- forward_engineering/ddlProvider.js
+++ forward_engineering/ddlProvider.js
@@ -117,6 +117,7 @@ module.exports = (baseProvider, options, app) => {
 				tableName: data.entityName,
 				alias: data.alias,
 				isActivated: data.isActivated,
+				dbName: data.dbName,
 			};
 		},
 
