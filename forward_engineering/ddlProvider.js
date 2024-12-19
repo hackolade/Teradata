d3870e4496099aa4f4827d2d8caf2333e6588910
Alik <alik.rahmonov@gmail.com>
2024-12-19 17:57:29 +0100

    HCK-9173: comment out inactive schema statement in script (#41)
    * HCK-9173: comment out inactive schema statement in script
    
    * fix

--- forward_engineering/ddlProvider.js
+++ forward_engineering/ddlProvider.js
@@ -164,7 +164,7 @@ module.exports = (baseProvider, options, app) => {
 
 		createDatabase({
 			databaseName,
-			isActivated,
+			isActivated = true,
 			db_account,
 			db_default_map,
 			db_permanent_storage_size,
@@ -187,21 +187,19 @@ module.exports = (baseProvider, options, app) => {
 				db_default_journal_db,
 			});
 
-			const databaseStatement = commentIfDeactivated(
-				assignTemplates(templates.createDatabase, {
-					name: databaseName,
-					databaseOptions,
-				}),
-				{
-					isActivated,
-				},
-			);
+			const databaseStatement = assignTemplates(templates.createDatabase, {
+				name: databaseName,
+				databaseOptions,
+			});
 
 			const createSessionStatement = assignTemplates(templates.createSession, {
 				name: databaseName,
 			});
 
-			return [databaseStatement, createSessionStatement].join('\n');
+			return commentIfDeactivated(
+				[databaseStatement, createSessionStatement].join('\n'),
+				{ isActivated },
+			);
 		},
 
 		createUdt(udt, dbData) {
