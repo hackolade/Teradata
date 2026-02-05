463564b33b86b154ea41b9a2cb727aa3d1f34fea
chulanovskyi-bs <56116665+chulanovskyi-bs@users.noreply.github.com>
2026-02-05 15:29:28 +0200

    fix: hydrate column JSON schema to resolve types (#67)

--- forward_engineering/ddlProvider.js
+++ forward_engineering/ddlProvider.js
@@ -1120,5 +1120,14 @@ module.exports = (baseProvider, options, app) => {
 				this.createView(alterData, dbData, true),
 			].join('\n\n');
 		},
+
+		hydrateJsonSchemaColumn(jsonSchema, definitionJsonSchema) {
+			if (!jsonSchema.$ref || _.isEmpty(definitionJsonSchema)) {
+				return jsonSchema;
+			}
+
+			jsonSchema = _.omit(jsonSchema, '$ref');
+			return { ...definitionJsonSchema, ...jsonSchema };
+		},
 	});
 };
